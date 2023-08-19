dacfad5fcbec77ffadfe010a3f180f403be33b34
Logan M Roe <Lmproe27@gmail.com>
2023-08-19 10:44:15 -0500

    freeme

--- src/pages/3 Parks near You.py
+++ src/pages/3 Parks near You.py
@@ -13,38 +13,25 @@ import myfuncs as mf
 c=ToMongo()
 
 #grab my collection
-cursor=c.park_info.find()
+cursor=c.park_info.find({ "longitude" : {"$ne" :None},"latitude" : {"$ne" :None}})
 
 #list into a dataframe
 df =  pd.DataFrame(list(cursor))
-#cleanse all nulls on the long and lat
 
-#grab the stats list for possible values
-
+#generate Possible values of states
 states=mf.pos_values(df,'states')
 
-
 st.header("Parks by State")
+#set options to pos values func
+state=st.selectbox("Select a State:",options=states)
 
-state=st.selectbox("Select a state:",options=states)
+#generate map based off the locator return value
+st.map(df[['latitude','longitude']].iloc[mf.locator(df,'states',state)],color="#39FF14")
 
+#create a clean string that plays nice in data_editor
+df['States_in_Park'] = mf.stringConvert(df,'states')
 
-def locator (df,col,val):
-    a_list=[]
-    for i in range(len(df[col])):
-        if type(df[col][i])==list:
-            if val in df[col][i]:
-                a_list.append(i)
-        else:
-            if val == df[col][i]:
-                a_list.append(i)
-    #return the list of locations to be used as a .iloc filter
-    return a_list
-
-
-
-st.map(df[['latitude','longitude']].iloc[locator(df,'states',state)],color="#39FF14")
-
-result = st.data_editor(df['full_name'].iloc[mf.locator(df,'states',state)],hide_index=True,width=800)
+#create a cleane list to see the info generated
+result = st.data_editor(df[['full_name','States_in_Park']].iloc[mf.locator(df,'states',state)],hide_index=True,width=800)
 
 
